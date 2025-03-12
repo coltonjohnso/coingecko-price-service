1c9eac4307063fa99471865da35f9378954221f5
coltonjohnso <coltonjohnso@icloud.com>
2025-03-12 02:18:58 -0400

    Update config list

--- src/config/coingecko-api-ids.ts
+++ src/config/coingecko-api-ids.ts
@@ -1,9 +1,4 @@
 // Set list of desired cryptocurrencies to fetch prices for
 export const API_ID_LIST = [
-    'bitcoin',
-    'kaspa',
     'quai-network',
-    'bitcoin-cash',
-    'bitcoin-sv',
-    'ethereum',
 ]; 
